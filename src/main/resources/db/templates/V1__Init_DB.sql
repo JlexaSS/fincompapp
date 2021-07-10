f43ec4a5a705416d87ebb4b2cb4d911033735072
JlexaSS <jowe.jlexa@mail.ru>
2021-07-11 01:57:43 +0300

    ффф

--- src/main/resources/db/templates/V1__Init_DB.sql
+++ src/main/resources/db/templates/V1__Init_DB.sql
@@ -1,29 +1,26 @@
-create schema fincompapp;
-
 create table cart (
     id bigint not null,
     count integer not null,
     non_user varchar(255),
     id_product bigint not null,
     id_user bigint,
-    primary key (id)
-);
+    primary key (id)) engine=InnoDB;
 
 create table categories (
     id bigint not null,
     description varchar(255) not null,
     name varchar(255) not null,
-    primary key (id)
-);
+    primary key (id)) engine=InnoDB;
 
-create sequence hibernate_sequence start 1 increment 1;
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+
+insert into hibernate_sequence values ( 1 );
 
 create table producers (
     id bigint not null,
     country varchar(255) not null,
     name varchar(255) not null,
-    primary key (id)
-);
+    primary key (id)) engine=InnoDB;
 
 create table product (
     id bigint not null,
@@ -33,30 +30,27 @@ create table product (
     price integer not null,
     id_category bigint not null,
     id_producer bigint not null,
-    primary key (id)
-);
+    primary key (id)) engine=InnoDB;
 
 create table user_roles (
     user_id bigint not null,
-    roles varchar(255) not null
-);
+    roles varchar(255) not null) engine=InnoDB;
 
 create table users (
     id bigint not null,
     active bit not null,
     password varchar(255) not null,
     username varchar(255) not null,
-    primary key (id)
-);
+    primary key (id)) engine=InnoDB;
 
-alter table if exists cart add constraint cart_product_fk foreign key (id_product) references product (id);
+alter table cart add constraint cart_product_fk foreign key (id_product) references product (id);
 
-alter table if exists cart add constraint cart_user_fk foreign key (id_user) references users (id);
+alter table cart add constraint cart_user_fk foreign key (id_user) references users (id);
 
-alter table if exists product add constraint product_category_fk foreign key (id_category) references categories (id);
+alter table product add constraint product_category_fk foreign key (id_category) references categories (id);
 
-alter table if exists product add constraint product_producer_fk foreign key (id_producer) references producers (id);
+alter table product add constraint product_producer_fk foreign key (id_producer) references producers (id);
 
-alter table if exists user_roles add constraint roles_user_fk foreign key (user_id) references users (id);
+alter table user_roles add constraint roles_user_fk foreign key (user_id) references users (id);
 
 
